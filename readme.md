e11cbfabb59e40843e7b3e6f11e133603973b2eb
emmett <xilibi2003@gmail.com>
2019-03-26 15:10:03 +0800

    First commit.

---
+++ readme.md
@@ -0,0 +1,17 @@
+## 强制条件
+
+安装Ganache、 Truffle、 MetaMask
+
+1. 安装库
+npm install 
+
+2. 启动Ganache 
+
+3. 编译部署 
+truffle migrate
+
+4. 启动
+npm run dev 
+
+5. http://localhost:3000 查看效果
+   查看其它人的账号 http://localhost:3000?account=账号地址
---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "pluck episode bubble crush dress kiss alien output cycle piano powder monster";
+
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: 5777 // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "ropsten.infura.io/v3/d3fe47cdbf454c9584113d05a918694f")
+      },
+      network_id: 3
+    } 
+  }
+};
