13d3d69d2575c09776de602cfc7c3ab2c90dfdf0
emmett <xilibi2003@gmail.com>
2019-03-30 20:18:49 +0800

    适配MetaMask 新特性。

--- src/js/app.js
+++ src/js/app.js
@@ -10,9 +10,25 @@ App = {
   },
 
   initWeb3: async function() {
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
-    } else {
+
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:9545');
     }
     web3 = new Web3(App.web3Provider);
